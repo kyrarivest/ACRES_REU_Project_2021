d84082ad1912ff704a09eb65ddc1487ced51a843
Kyra Rivest <f0102398@dev-intel14-k20.i>
2021-08-02 15:33:14 -0400

    code organization

---
+++ examples/cgenffe5/save_results.py
@@ -0,0 +1,35 @@
+import json
+import sys
+
+file = open("results.json", "r")
+scores = json.load(file)
+file.close()
+
+print("Training times: " + str(scores[0]))
+print("R Squared Values: " + str(scores[1]))
+print("RMSE Values: " + str(scores[2]))
+
+avgs = []
+
+pcc = 0
+for i in range(len(scores['PCC']) - 5, len(scores['PCC'])):
+    pcc += scores['PCC'][i]
+
+avgs.append((pcc/5))
+
+rmse = 0
+for i in range(len(scores['PCC']) - 5, len(scores['RMSE'])):
+    rmse += scores['RMSE'][i]
+
+avgs.append((rmse/5))
+
+
+
+#Code ot save results to total list
+num_comps = str(sys.path[0])[len("/mnt/ufs18/home-088/f0102398/ClassicalGSG/examples/"):]
+
+file = open("/mnt/home/f0102398/ClassicalGSG/examples/results.txt", "a")
+file.write("\n")
+file.write(num_comps + "\n")
+file.write(str(avgs))
+file.close()
